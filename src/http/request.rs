d231ba838da5a2d6150f7178bf6c9e5e38fbefe4
Minimega12121 <architdabral1234567890@gmail.com>
2024-05-16 23:26:48 +0530

    progess

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,8 +1,117 @@
-use super::method::Method;
+use crate::http::request;
 
+use super::method::{Method,MethodError};
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Result as FmtResult,Formatter,Display,Debug};
+use std::str;
+use std::str::Utf8Error;
 //enum is a special type with finite set of values 
 pub struct Request {
     path : String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n...HEADERS...
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        // match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+        //     Ok(request) => {},
+        //     Err(e) => return Err(e),
+        // }
+        //same as above
+
+        let request = str::from_utf8(buf)?;
+        
+        // match get_next_word(request) {
+        //     Some((method, request)) => {},
+        //     None => return Err(ParseError::InvalidRequest),
+        // }
+        //same as above
+
+        let (method,request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, __) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
+        let mut query_string = None;
+
+        // match path.find('?') {
+        //     Some(i) => {
+        //         query_string = Some(&path[i+1..]);
+        //         path = &path[..i];
+        //     },
+        //     None => {},//We dont care for this case
+        // };
+        
+        //same as above, used when we only care about one case and dont care about other
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i+1..]);
+            path = &path[..i];
+        }
+
+        unimplemented!()
+    }
+}
+fn get_next_word(request: &str) -> Option<(&str, &str)>{
+    for (i, c) in request.chars().enumerate(){
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i+1..]));
+        }
+    }
+    None
+}
+
+impl ParseError{
+    fn message(&self) -> &str{
+        match self{
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl From<MethodError> for ParseError{
+    fn from(_: MethodError) -> Self{
+        Self::InvalidMethod
+}
+}
+
+impl From<Utf8Error> for ParseError{
+    fn from(_: Utf8Error) -> Self{
+        Self::InvalidEncoding
+    }
+}
+
+impl Display for ParseError{
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult{
+        write!(f, "{}", self.message())
+    }
+}
+
+
+impl Debug for ParseError{
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult{
+        write!(f, "{}", self.message())
+    }
+}
+
+pub enum ParseError{
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+
+impl Error for ParseError{}
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+
 
 pub struct Server {
     addr: String,
@@ -23,7 +26,21 @@ impl Server {
                 Ok((mut stream, _)) => {
                     //For this simmple server 1024 bytes is enough
                     let mut buffer = [0; 1024];
-                    stream.read(&mut buffer);
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+                            // try from is used for converstion of one type to another
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => {
+                                    eprintln!("Failed to parse a request: {}", e);
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("Failed to read from connection: {}", e);
+                        }
+                    }
                 },
                 Err(e) => {
                     eprintln!("Error accepting connection: {}", e);
